0e2f36c1e82ccdb684f9186129d424af7e4c2273
hktrollgames <hktrollgames@gmail.com>
2022-07-24 13:39:04 +0900

    블럭 생성

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,18 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector2Int pos;
+    public SpriteRenderer sr;
 }
---
+++ Assets/BlockGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    //블럭 자동 생성
+    public Block baseBlock;
+    public int MaxX = 7;
+    public int MaxY = 5;
+
+    [ContextMenu("생성")]
+    public void GenerateBlocks()
+    {
+        for (int y = 0; y < MaxY; y++)
+        {
+            for (int x = 0; x < MaxX; x++)
+            {
+                Block newBlock = Instantiate(baseBlock);
+                //newBlock.pos = new Vector2Int(x, y);
+                newBlock.transform.position = new Vector3(x, y, 0);
+                newBlock.GetComponent<SpriteRenderer>().sprite =
+                    sprites[Random.Range(0, sprites.Count)];
+            }
+        }
+    }
+}
